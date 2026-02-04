9c8d75092e7fc8e514d722815cfc1a8f8fa71a5b
Pillowe <pillowe@163.com>
2026-02-04 23:26:05 +0800

    服务器端数据库换用mysql

--- config/settings.py
+++ config/settings.py
@@ -75,13 +75,28 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_TYPE') != 'MYSQL':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'vigorous_db',
+            'USER': 'vigorous_user',
+            'PASSWORD': 'vigorous',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+    }
+    }
+    
 
 
 # Password validation
